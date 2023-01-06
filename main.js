15521e9bbb0fde3530b4873dc829de0b7144aeda
Bruno Seiji <88354853+bruno-seiji@users.noreply.github.com>
2023-01-06 10:52:47 -0500

    .

--- main.js
+++ main.js
@@ -18,11 +18,14 @@ var b=0
 confirmar.addEventListener("click", () => {
     
     if (num_pares.value > 1) {
-        criar_tabela();
-        num_pares.setAttribute("readonly",true);
+        criar_tabela()
+        num_pares.setAttribute("readonly",true)
         confirmar.classList.add("hidden")
+        confirmar.classList.remove("show")
         reset.classList.add("show")
+        reset.classList.remove("hidden")
         calcular.classList.add("show")
+        calcular.classList.remove("hidden")
     }
     else {window.alert("Utilizar no minimo 2 pares (X/Y)")}
 
@@ -30,15 +33,24 @@ confirmar.addEventListener("click", () => {
 })
 
 calcular.addEventListener("click", ()=>{
-  definir_arrays();
-  definir_array_quadrados();
-  definir_arrayXY();
-  definir_A_B();
-  console.log(a);
-  console.log(b);
+  definir_arrays()
+  definir_array_quadrados()
+  definir_arrayXY()
+  definir_A_B()
+  console.log(a)
+  console.log(b)
 })
 
-
+reset.addEventListener("click", () => {
+  document.querySelector("table").remove()
+  confirmar.classList.add("show")
+  confirmar.classList.remove("hidden")
+  reset.classList.add("hidden")
+  reset.classList.remove("show")
+  calcular.classList.add("hidden")
+  calcular.classList.remove("show")
+  num_pares.removeAttribute("readonly",true)
+})
 
 
 function criar_tabela(){
@@ -50,10 +62,9 @@ function criar_tabela(){
   novaTabela.appendChild(linha_tabela)
   linha_tabela.appendChild(col1)
   linha_tabela.appendChild(col2)
-
-
   col1.textContent = "X"
   col2.textContent = "Y"
+  
   for (let i=0; i<num_pares.value; i++)
   {
     const linha_tabela= document.createElement("tr")
@@ -64,10 +75,11 @@ function criar_tabela(){
       const input = document.createElement("input")
       linha_tabela.appendChild(col)
       col.appendChild(input)
-
+      input.setAttribute("type","number")
+      input.setAttribute("value","0")
     }
   }
-}
+ }
 
 function definir_A_B(){
     b = ((num_pares.value*arrayXY[num_pares.value])-(arrayX[num_pares.value]*arrayY[num_pares.value]))/((num_pares.value*arrayX_quadrado[num_pares.value])-(arrayX[num_pares.value]*arrayX[num_pares.value]))
@@ -100,16 +112,13 @@ function definir_arrays(){
   arrayX[num_pares.value] = 0
   arrayY[num_pares.value] = 0
   while (w < num_pares.value)
-  { arrayX[w] = parseInt(document.querySelectorAll("input")[i].value);
-    arrayX[num_pares.value] = arrayX[num_pares.value] + parseInt(document.querySelectorAll("input")[i].value);
-    arrayY[w] = parseInt(document.querySelectorAll("input")[e].value);
-    arrayY[num_pares.value] = arrayY[num_pares.value] + parseInt(document.querySelectorAll("input")[e].value);
-    i = i+2;
-    w++;
-    e = e+2;
+  { arrayX[w] = parseInt(document.querySelectorAll("input")[i].value)
+    arrayX[num_pares.value] = arrayX[num_pares.value] + parseInt(document.querySelectorAll("input")[i].value)
+    arrayY[w] = parseInt(document.querySelectorAll("input")[e].value)
+    arrayY[num_pares.value] = arrayY[num_pares.value] + parseInt(document.querySelectorAll("input")[e].value)
+    i = i+2
+    w++
+    e = e+2
   }
 }
-
-
-
 
